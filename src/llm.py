a85c53be367cbe50d183bdf1c24f5e6a1f6b4f84
Ali Altunoğlu <alialtunoglu02@gmail.com>
2026-01-29 20:28:42 +0300

    Refactor agent and LLM setup: add plotting tool and update LLM model version

--- src/llm.py
+++ src/llm.py
@@ -7,7 +7,7 @@ def get_llm():
         raise ValueError("GOOGLE_API_KEY .env dosyasında bulunamadı!")
     
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         temperature=0, # SQL sorguları için 0 olması önemlidir
         google_api_key=GOOGLE_API_KEY,
         convert_system_message_to_human=True
